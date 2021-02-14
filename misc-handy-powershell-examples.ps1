15789ae3e888283e078211e2fe92c874d479e4e8
Matt Seng <mmseng@users.noreply.github.com>
2021-02-13 21:52:01 -0600

    Update misc-handy-powershell-examples.ps1

--- misc-handy-powershell-examples.ps1
+++ misc-handy-powershell-examples.ps1
@@ -447,11 +447,21 @@ $speaker.Dispose()
 
 # Remove all stored credentials from Credential Manager relating to the university
 # Useful to clear out credentials when you've changed your NetID password
+# Note cmdkey.exe has a bug which causes it to fails removing credentials which contain parentheses, or combinations of spaces and hyphens,
+# these will have to be removed manually.
 
 # Define wildcard queries to target desired credentials
 $targets = @(
-	"*illinois.edu*",
-	"*uofi*"
+    "*mseng3*",
+    "*illinois.edu*",
+    "*uofi*",
+    "*engrit*",
+    "*gelib-idea*",
+    "*ews*",
+    "*office365*",
+    "*MicrosoftOffice*",
+    "*msteams*",
+    "*OneDrive*"
 )
 
 # Get all credential items
@@ -460,19 +470,22 @@ $items = cmdkey /list | Where { $_ -like "*Target:*" }
 # Get all credential items which match queries
 $targetItems = @()
 foreach($item in $items) {
-	foreach($target in $targets) {
-		if($item -like $target) {
-			$targetItems += $item
-		}
-	}
+    foreach($target in $targets) {
+        if($item -like $target) {
+            $targetItems += $item
+        }
+    }
 }
 
 # For each matching item
 foreach($item in $targetItems) {
-	$item = $item.Replace(" ","")
-	$item = $item.Replace("Target:","")
-	Write-Host $item
-	#cmdkey /del:$target
+    $item = $item.Replace("Target:","")
+    $item = $item.Replace("LegacyGeneric:","")
+    $item = $item.Replace("Domain:","")
+    $item = $item.Replace("target=","")
+    $item = $item.Trim()
+    Write-Host $item
+    cmdkey /del:$item
 }
 
 # -----------------------------------------------------------------------------
