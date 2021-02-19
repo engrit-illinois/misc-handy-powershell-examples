c82cf0ae0057ed311eadb0ce19a9eaa3a2d45180
Matt Seng <mmseng@users.noreply.github.com>
2021-02-18 21:54:31 -0600

    Update misc-handy-powershell-examples.ps1

--- misc-handy-powershell-examples.ps1
+++ misc-handy-powershell-examples.ps1
@@ -295,7 +295,7 @@ function log {
 
 			# Uncomment one of these depending on whether output goes to the console by default or not, such that the user can override the default
 			#if($ConsoleOutput) {
-			if(!$NoConsoleOutput)
+			if(!$NoConsoleOutput) {
 			
 				# If we're allowing console output, then Write-Host
 				if($NoNL) {
