f31cd5dc05af827c24caef0ccf5da784ef687589
Matt Seng <mmseng@users.noreply.github.com>
2021-07-15 16:58:53 -0500

    Update misc-handy-powershell-examples.ps1

--- misc-handy-powershell-examples.ps1
+++ misc-handy-powershell-examples.ps1
@@ -781,7 +781,7 @@ $monitors = foreach($int in @(1..39)) {
     $name = "dcl-l520-$num"
     Get-CimInstance -ComputerName $name -Class Win32_VideoController
 }
-$monitors | Where { $_.Caption -notlike "*remote*" } | Select PSComputerName,Caption,CurrentHorizontalResolution,CirrentVerticalResolution,VideoModeDescription | Format-Table 
+$monitors | Where { $_.Caption -notlike "*remote*" } | Select PSComputerName,Caption,CurrentHorizontalResolution,CurrentVerticalResolution,VideoModeDescription | Format-Table 
 
 # -----------------------------------------------------------------------------
 
